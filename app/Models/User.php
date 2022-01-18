ae469c089f1c672f62efc97f55479ef8830e9d97
Victor Reeves <dr.vreeves@gmail.com>
2022-01-18 23:09:16 +0100

    second commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,16 +12,7 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $guarded = [];
 
     /**
      * The attributes that should be hidden for serialization.
@@ -41,4 +32,17 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function profile()
+    {
+        return $this->hasOne(Profile::class);
+    }
+
+    protected static function booted()
+    {
+        static::created(function ($user) {
+            $profile = Profile::create(['user_id' => $user->id]);
+        });
+    }
+
 }
